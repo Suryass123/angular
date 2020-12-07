232f8400987beaaaa116086c5229157a22545f84
Suryass123 <suryaravindren@gmail.com>
2020-12-07 21:27:45 +0530

    Update app.module.ts

--- mytest/src/app/app.module.ts
+++ mytest/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-//import hai hai
+//import hai hai hai hai
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
